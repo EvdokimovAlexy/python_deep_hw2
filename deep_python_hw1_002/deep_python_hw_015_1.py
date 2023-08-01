888821216576601079cdbd74f2571c0aa2ddf3ca
EvdokimovAlexy <112196835+EvdokimovAlexy@users.noreply.github.com>
2023-08-01 13:35:59 +0300

    group_rename_1

---
+++ deep_python_hw1_002/deep_python_hw_015_1.py
@@ -0,0 +1,6 @@
+import deep_python_hw_015_2 as hf
+
+if __name__ == '__main__':
+    print("Переименование txt файлов в каталоге data_files")
+    files_cnt = hf.group_rename_files("_fn_", ".txt", path="./data_files")
+    print(f"Переименовано: {files_cnt}")
